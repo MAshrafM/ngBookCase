44d3bbf0cfa82bd55e6fdc03b1faed440fdd710d
MAshraf <ee.mashraf@gmail.com>
2018-10-02 16:00:52 +0200

    config app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { BooksComponent } from './books/books.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'books', component: BooksComponent},
+  {path: 'about', component: AboutComponent},
+];
 
 @NgModule({
   imports: [
-    CommonModule
+    RouterModule.forRoot(routes)],
   ],
-  declarations: []
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
 
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
 import { AppComponent } from './app.component';
 import { BooksComponent } from './books/books.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { BooksService } from './books/books.service'
+
 @NgModule({
   declarations: [
     AppComponent,
     BooksComponent,
     HomeComponent,
-    AboutComponent
+    AboutComponent,
+    NavbarComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    NgbModule.forRoot(),
+    AngularFontAwesomeModule,
   ],
-  providers: [],
+  providers: [BooksService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
